465863295a2946a8eb848f7c2c378d3fe497d2e5
張家銘 <vincent191228@gmail.com>
2018-10-13 18:47:22 +0800

    set env PATH complete

--- npshell.cpp
+++ npshell.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
+
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 using namespace std;
 
+void initenv(){
+    char buff[80];
+    getcwd(buff,sizeof(buff));
+    chroot(buff);
+    //chdir("/");
+    setenv("PATH","bin:.",1);
+    
+}
 
+void printenv(){
+    cout<<getenv("PATH")<<"\n";
+}
 int main()
 {
 string command;
 cout<< "test command please\n";
 while (command != "exit")
-{
+{  
+    cout<<"%";
+    cin>>command;
+    initenv();
+ 
     if (command == "printenv")
-        system("printenv");
+        printenv();
     else if (command == "ls")
         system("ls");
     else if (command=="env")
         system("env");
-cout<<"%";
-cin>>command;
+    else if (command == "cat")
+        system("cat");
+    else 
+        cout<<"ERROR\n";
 }
 return 0;
 }
